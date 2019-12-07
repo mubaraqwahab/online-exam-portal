1e8b8d1981074c8d14776b2b05a3ad783f1f0687
Esat <yamanesad.ey@gmail.com>
2019-12-07 23:19:02 +0100

    connect.php succesfully created

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "examportal";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
