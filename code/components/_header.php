a104d5e6891aa1388ad8a6fc1e9da2886a5d64da
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-12-13 17:58:53 +0100

    Deconstruct index.html into php components

---
+++ code/components/_header.php
@@ -0,0 +1,10 @@
+<?php
+echo '
+<header class="text-center py-2">
+  <a href="https://nileuniversity.edu.ng/" class="d-inline-flex content-center">
+    <img src="img/nile.png" height="40px" width="40px">
+    <span class="h5 ml-2 mb-0">Online Exam Portal</span>
+  </a>
+</header>
+'
+?>
---
+++ code/components/_sidebar.php
@@ -0,0 +1,13 @@
+<?php
+echo '
+<div class="bg-light border-right" id="sidebar-wrapper">
+  <div class="list-group list-group-flush sticky-top pt-2">
+    <h5 class="sidebar-heading">Dashboard</h5>
+    <a href="#" class="list-group-item list-group-item-action bg-light" id="">Assessment</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light" id="">My Quiz</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Exam</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Create Exam</a>
+  </div>
+</div>
+'
+?>
