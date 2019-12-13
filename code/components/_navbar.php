5a0a985673aa68d5e33ba99e7113f9b05359d8ce
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-12-13 17:58:53 +0100

    Deconstruct index.html into php components

---
+++ code/components/_navbar.php
@@ -0,0 +1,23 @@
+<?php
+echo '
+<nav class="navbar navbar-light sticky-top bg-light border-bottom">
+  <div class="d-flex align-items-center">
+    <button class="btn btn-light" id="menu-toggle"><i class="fa fa-bars"></i></button>
+    <h4 class="ml-3 mb-0" id="pageTitle">Page Title</h4>
+  </div>
+
+  <div class="dropdown">
+    <button class="btn" id="profileDropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <span class="d-none d-md-inline-block mr-2">Username</span>
+      <img src="img/avatar2.png" height="30px" width="30px">
+    </button>
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="profileDropdown">
+      <a class="dropdown-item" href="#">View Profile</a>
+      <a class="dropdown-item" href="#">Edit Profile</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Sign Out</a>
+    </div>
+  </div>
+</nav>
+'
+?>
---
+++ code/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Home</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="css/simple-sidebar.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <?php include 'components/_header.php' ?>
+
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <?php require 'components/_sidebar.php' ?>
+
+    <!-- Page content wrapper -->
+    <div id="page-content-wrapper">
+
+      <!-- Navbar -->
+      <?php require 'components/_navbar.php' ?>
+
+      <!-- Page content -->
+      <div class="container"></div>
+
+    </div>
+
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+  <!-- Custom Script -->
+  <script>
+    // Sidebar toggle
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+
+    // Page title
+    var pageTitle = $(document).attr("title");
+    $("#pageTitle").text(pageTitle);
+
+    // Active sidebar menu item
+    // if (pageTitle.toLowerCase() == $('#assessment').attr('id')) {
+    //   $('#assessment').addClass('active');
+    // }
+  </script>
+</body>
+
+</html>
