cbe115ade16eb58daed58192db158c26bc409fa6
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-12-16 23:06:49 +0100

    Complete most of create form UI
    Create separate JS file

--- code/create-exam.php
+++ code/create-exam.php
@@ -13,81 +13,73 @@
   <title>Create Exam</title>
 </head>
 
-<body>
-  <h2>Create Exam</h2>
+<body class="container">
+  <h2 id="createExamTitle">Create Exam</h2>
 
   <form method="post">
-    <fieldset id="create-1">
+    <fieldset id="create1" class="mt-4">
 
       <div class="form-group">
-        <label for="noOfQuestions">How many questions does the exam have?</label>
-        <input type="number" class="form-control" name="noOfQuestions" id="noOfQuestions">
-      </div>
-      <div class="form-group">
-        <label for="examType">What type of exam is it?</label>
-        <select class="form-control" name="examType" id="examType">
-          <option value="">Multi-choice</option>
-          <option value="">Theory</option>
-          <option value="">Fill in the blank</option>
+        <label for="course">Course</label>
+        <select class="custom-select" name="courseCode" id="courseCode" required>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
         </select>
       </div>
 
-      <button type="button" data-direction="next" class="btn btn-primary">Next</button>
+      <div class="form-group">
+        <label for="examTitle">Title</label>
+        <input type="text" class="form-control" name="examTitle" id="examTitle" placeholder="e.g. Mid-term Exam" required>
+      </div>
+
+      <div class="form-group">
+        <label for="noOfQuestions">How many questions does the exam have?</label>
+        <input type="number" class="form-control" name="noOfQuestions" id="noOfQuestions" value="1" required>
+      </div>
+
+      <div class="form-group">
+        <label for="examType">What type of exam is it?</label>
+        <select class="custom-select" name="examType" id="examType" required>
+          <option value="1">Multi-choice</option>
+          <option value="2">Fill in the blank</option>
+          <option value="3">Theory</option>
+        </select>
+      </div>
+
+      <button type="button" data-direction="next" class="btn btn-primary mt-3">Next</button>
+      <button type="reset" class="btn btn-danger mt-3" onClick="history.go(0);">Reset</button>
     </fieldset>
 
-    <fieldset id="create-2">
-      <div class="form-group question-group">
-        <label for="exampleInputEmail1">Question</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div class="form-group row">
-          <label for="inputPassword" class="col-sm-2 col-form-label">A</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword">
-          </div>
+    <fieldset id="create2" class="mt-4">
+      <div id="createQuestion"></div>
 
-        </div>
-        <div class="form-group row">
-          <label for="inputPassword" class="col-sm-2 col-form-label">B</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword">
-          </div>
+      <button type="button" data-direction="next" class="btn btn-primary mt-3">Next</button>
+      <button type="reset" class="btn btn-danger mt-3" onClick="history.go(0);">Reset</button>
+    </fieldset>
 
-        </div>
-        <div class="form-group row">
-          <label for="inputPassword" class="col-sm-2 col-form-label">C</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword">
+    <fieldset id="create3" class="mt-4">
+      <div class="form-group">
+        <p>Share this link to invite students to take the exam:</p>
+        <p class="h5 border p-2 d-inline-block">qS31kUil</p>
+      </div>
+      <p class="my-3">OR</p>
+      <div class="form-group">
+        <label for="inviteByEmail">Invite them by their emails:</label>
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" id="inviteByEmail" aria-describedby="inviteButton">
+          <div class="input-group-append">
+            <button class="btn btn-outline-primary" type="button" id="inviteButton">Invite</button>
           </div>
-
-        </div>
-        <div class="form-group row">
-          <label for="inputPassword" class="col-sm-2 col-form-label">D</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword">
-          </div>
-
-        </div>
-        <div class="form-group row">
-          <label for="inputPassword" class="col-sm-2 col-form-label">The correct option is </label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword">
-          </div>
-
         </div>
       </div>
-      <!-- <p>This is the first question</p>
-        <input type="text" class="form-control"> -->
+      <div class="form-group" id="inviteesList">
+        <h5>Invitees</h5>
+        <ul class="list-group"></ul>
+      </div>
 
-      <button type="button" data-direction="previous" class="btn btn-primary">Previous</button>
-      <button type="button" data-direction="next" class="btn btn-primary">Next</button>
-    </fieldset>
-
-    <fieldset id="create-3">
-      <p>This is the second question</p>
-      <input type="text" class="form-control">
-
-      <button type="button" data-direction="previous" class="btn btn-primary">Previous</button>
-      <button type="submit" name="submit" class="btn btn-primary">Finish</button>
+      <button type="submit" name="submit" class="btn btn-success mt-3">Finish</button>
+      <button type="button" class="btn btn-danger mt-3" onClick="history.go(0);">Reset</button>
     </fieldset>
 
   </form>
@@ -106,22 +98,8 @@
     integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
   </script>
 
-  <script>
-  $("fieldset:not(:first-child)").hide();
-
-  $("fieldset [data-direction='next']").on('click', function(e) {
-    $(this).parent().hide().next().show();
-
-    for (i = 0; i < $('input#noOfQuestions').val(); i++) {
-      // TODO: Change id names (increment no)
-      $('.question-group').clone().appendTo("#create-2");
-    }
-  });
-
-  $("fieldset [data-direction='previous']").on('click', function(e) {
-    $(this).parent().hide().prev().show();
-  });
-  </script>
+  <!-- Custom JS -->
+  <script src="js/script.js"></script>
 </body>
 
 </html>
