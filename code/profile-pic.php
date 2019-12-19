a64f1a26b25fc30169749d21b832719d352b1eb6
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-12-19 14:16:25 +0100

    Mini updates
    Make profile pic and username dynamic
    Add randomness to invite code

---
+++ code/profile-pic.php
@@ -0,0 +1,14 @@
+<?php
+
+  include 'connect.php';
+
+  $id = $_GET['id'];
+  // do some validation here to ensure id is safe
+
+  $sql = "SELECT profile_picture FROM user WHERE user_id='$id'";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc();
+
+  header("Content-type: image/jpeg");
+  echo $row['profile_picture'];
+?>
