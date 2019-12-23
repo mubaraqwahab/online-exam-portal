8706afb738293ea0d6f2b04c4a3fe2e416d95f10
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-12-24 00:28:54 +0100

    Fix empty profile pic issue

--- code/components/_navbar.php
+++ code/components/_navbar.php
@@ -7,7 +7,9 @@
   <div class="dropdown">
     <button class="btn" id="profileDropdown" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <span class="d-none d-md-inline-block mr-1"><?php echo isset($userId) ? $userId : "User ID"; ?></span>
-      <img src="../profile-pic/<?php echo (!file_exists(PROFILE_TARGET_DIR . $profilePicture) ? 'blank-square.jpg' : $profilePicture); ?>" class="rounded-circle" height="30px" width="30px">
+      <img src="../profile-pic/<?php
+        echo (empty($profilePicture) || !file_exists(PROFILE_TARGET_DIR . $profilePicture) ? 'blank-square.jpg' : $profilePicture); ?>"
+        class="rounded-circle" height="30px" width="30px">
     </button>
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="profileDropdown">
       <div class="dropdown-item d-md-none"><?php echo isset($userId) ? $userId : "User ID"; ?></div>
