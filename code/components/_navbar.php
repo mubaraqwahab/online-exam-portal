3880ebb39d035909a73470acec88454e50fc091e
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-12-27 08:40:25 +0100

    Remove sign out.php

--- code/components/_navbar.php
+++ code/components/_navbar.php
@@ -16,8 +16,8 @@
       <div class="dropdown-divider d-md-none"></div>
       <a class="dropdown-item" href="/online-exam-portal/code/sign/profile.php">Edit Profile</a>
       <div class="dropdown-divider"></div>
-      <form method="post" action="../sign/sign-out.php" class="m-0">
-        <button name="signOut" class="dropdown-item">Sign Out</button>
+      <form method="post" class="m-0">
+        <button name="signOut" type="submit" class="dropdown-item">Sign Out</button>
       </form>
     </div>
   </div>
