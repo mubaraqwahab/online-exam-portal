cfa5ed2fcae38c9cf5f1e5c969c04fd3cf5b0a93
Mubaraq Wahab <mub.wahab26@gmail.com>
2019-12-29 17:32:04 +0100

    ...

--- code/manage-exam/exam.php
+++ code/manage-exam/exam.php
@@ -179,9 +179,9 @@
 
       </div>
 
-    <!-- </div>
+    </div>
 
-  </div> -->
+  </div>
 
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
