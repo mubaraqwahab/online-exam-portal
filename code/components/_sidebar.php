4cd396cfa399f9bd26eca810d7f64ebb09aae695
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-01 16:55:21 +0100

    Add spacing to sidebar

--- code/components/_sidebar.php
+++ code/components/_sidebar.php
@@ -3,9 +3,11 @@
     <h5 class="sidebar-heading">Dashboard</h5>
     <div class="list-group list-group-flush">
       <a href="<?php echo ROOT_DIR . 'notifications/' ?>" class="list-group-item list-group-item-action bg-light" id="notifications">Notifications</a>
-      <span class="list-group-item bg-light"><b>For Instructors</b></span>
+
+      <span class="list-group-item bg-light mt-3"><b>For Instructors</b></span>
       <a href="<?php echo ROOT_DIR . 'create-exam/' ?>" class="list-group-item list-group-item-action bg-light" id="create-exam">Create Exam</a>
       <a href="<?php echo ROOT_DIR . 'manage-exam/' ?>" class="list-group-item list-group-item-action bg-light" id="manage-exams">Manage Exams</a>
+
       <span class="list-group-item bg-light mt-3"><b>For Students</b></span>
       <a href="<?php echo ROOT_DIR . 'take-exams/' ?>" class="list-group-item list-group-item-action bg-light" id="take-exams">Take Exams</a>
       <a href="<?php echo ROOT_DIR . 'submissions/' ?>" class="list-group-item list-group-item-action bg-light" id="submissions">Submissions</a>
