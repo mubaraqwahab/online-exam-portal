b4bb051106bbcaa8a57bb9b7b2e8a32d9f434e93
Esat <yamanesad.ey@gmail.com>
2020-01-03 23:29:36 +0100

    Done with the mark exam file

--- code/manage-exam/_mark-exam.php
+++ code/manage-exam/_mark-exam.php
@@ -1,15 +1,40 @@
 <?php
+include "../connect.php";
 
 $examID = $_POST['examID'];
 $assigneeID = $_POST['assigneeID'];
 
-// Select type id, no of questions from exam id
-// For each question,
-  // Get score
-  // If its fill in,
-    // Send to fill in table
-  // If its theory,
-    // Send to theory table
+$sql = "SELECT type_id, no_of_questions FROM exam WHERE exam_id = $examID";
 
-// Redirect to feedback page with success or failure param
+$result = mysqli_query($conn, $sql);
+
+
+$exam = mysqli_fetch_assoc($result);
+$typeID = $exam['type_id'];
+$noOfQuestion = $exam['no_of_questions'];
+
+$worked = TRUE;
+
+for ($i=1; $i<=$noOfQuestion; $i++){
+  $score = $_POST["score". $i];
+  if($typeID == 3){
+  $sql2 = "UPDATE theory_response SET score = $score WHERE exam_id = $examID AND assignee_id = $assigneeID AND question_no = $i";
+  }
+  else if($typeID == 2){
+    $sql2 = "UPDATE fill_in_response SET score = $score WHERE exam_id = $examID AND assignee_id = $assigneeID AND question_no = $i";
+    }
+  else{
+    $worked = FALSE;
+  }
+  $result2 = mysqli_query($conn, $sql2);
+
+}
+if($worked){
+  header ("Location: feedback.php?feedback=success");
+}
+else($worked){
+  header ("Location: feedback.php?feedback=failure");
+
+}
+
 ?>
