dc3856283efb15dd39fbc952e93c492c77c7d0d2
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-03 10:28:39 +0100

    Fix student validation bug

--- code/take-exams/multi-exam.php
+++ code/take-exams/multi-exam.php
@@ -38,10 +38,11 @@ $headerSql = "SELECT e.*, c.course_name, t.value AS type, CONCAT(u.first_name, '
   INNER JOIN user AS u ON u.user_id = e.instructor_id
   INNER JOIN `course` AS c ON c.course_code = e.course_code
   INNER JOIN exam_type AS t ON t.type_id = e.type_id
-  WHERE e.exam_id = ? AND e.instructor_id = ? AND e.type_id = ?";
+  WHERE e.exam_id = ? AND e.type_id = ?
+  AND EXISTS (SELECT 1 FROM exam_assignment WHERE exam_id = ? AND assignee_id = ?)";
 
 $headerStmt = $conn->prepare($headerSql);
-$headerStmt->bind_param('isi', $examID, $userID, $typeID);
+$headerStmt->bind_param('iisi', $examID, $typeID, $examID, $userID);
 $headerStmt->execute();
 $headerResult = $headerStmt->get_result();
 
