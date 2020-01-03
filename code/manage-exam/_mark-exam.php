6bd6c9b768fd6cce7535096ec28bce8cb1ee7866
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-03 22:51:30 +0100

    Add how tos to mark exam

--- code/manage-exam/_mark-exam.php
+++ code/manage-exam/_mark-exam.php
@@ -2,4 +2,13 @@
 
 // Get exam id and assignee id
 
+// Select type id, no of questions from exam id
+// For each question,
+  // Get score
+  // If its fill in,
+    // Send to fill in table
+  // If its theory,
+    // Send to theory table
+
+// Redirect to feedback page with success or failure param
 ?>
