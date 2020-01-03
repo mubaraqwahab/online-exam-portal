ee898682a6688db9fa5194abc259175da7bf7a45
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-03 12:41:35 +0100

    Complete take theory exam

--- code/take-exams/theory-exam.php
+++ code/take-exams/theory-exam.php
@@ -24,6 +24,47 @@ $profilePicture = $_SESSION['profilePicture'];
 
 </head>
 
+<?php
+
+// TODO: decode examID
+$examID = $_GET['examID'];
+
+$typeID = 3;
+
+// Get the exam details from database
+// Validate the student ID, exam ID and type ID as well
+$headerSql = "SELECT e.*, c.course_name, t.value AS type, CONCAT(u.first_name, ' ', u.last_name) AS instructor
+  FROM `exam` AS e
+  INNER JOIN user AS u ON u.user_id = e.instructor_id
+  INNER JOIN `course` AS c ON c.course_code = e.course_code
+  INNER JOIN exam_type AS t ON t.type_id = e.type_id
+  WHERE e.exam_id = ? AND e.type_id = ?
+  AND EXISTS (SELECT 1 FROM exam_assignment WHERE exam_id = ? AND assignee_id = ?)";
+
+$headerStmt = $conn->prepare($headerSql);
+$headerStmt->bind_param('iisi', $examID, $typeID, $examID, $userID);
+$headerStmt->execute();
+$headerResult = $headerStmt->get_result();
+
+// If no such exam, give error
+
+if ($headerResult->num_rows != 1) {
+  showError('Page not available.');
+  exit;
+}
+
+$exam = $headerResult->fetch_assoc();
+
+// Get questions from database
+
+$questionSql = "SELECT * FROM `theory_question` WHERE exam_id = ? ORDER BY question_no ASC";
+
+$questionStmt = $conn->prepare($questionSql);
+$questionStmt->bind_param('i', $examID);
+$questionStmt->execute();
+$questionResult = $questionStmt->get_result();
+?>
+
 <body>
 
   <?php include '../components/_header.php' ?>
@@ -45,12 +86,17 @@ $profilePicture = $_SESSION['profilePicture'];
         <!-- Heading -->
         <header class="mb-4" id="examHeader">
           <!-- Format: Mark [Course Code] ([Course Name]) [Exam Title] (Exam Type) -->
-          <h4>Mark CSC303 (Web) Midterm <small class="text-muted">(Theory)</small></h4>
+          <h4>
+            <?php
+              echo "{$exam['course_code']} ({$exam['course_name']}) {$exam['title']}
+              <small class='text-muted'>({$exam['type']})</small>";
+            ?>
+          </h4>
           <div class="d-flex flex-column flex-md-row">
             <div class="mr-md-3">
               <!-- PHP should put ID and Name here -->
-              <div>Instructor: Abdulhakeem Audu</div>
-              <div>10 Questions</div>
+              <div>Instructor: <?php echo $exam['instructor']; ?></div>
+              <div><?php echo $exam['no_of_questions']; ?> Question<?php echo pluralSuffix($exam['no_of_questions']); ?></div>
             </div>
           </div>
         </header>
@@ -58,42 +104,70 @@ $profilePicture = $_SESSION['profilePicture'];
         <!-- Form containing questions list -->
         <form method="POST" action="_submit-exam.php">
 
-          <!-- Each card is a question group -->
+          <?php
+            if ($questionResult->num_rows > 0) {
+              while ($question = $questionResult->fetch_assoc()) {
+                echo '
+                <div class="card my-3">
+                  <div class="card-body">
+                    <!-- Question no and marks should change -->
+                    <h5 class="card-title">
+                      Question '. $question['question_no'] . '
+                      <small>('. $question['mark'] .' Mark'. pluralSuffix($question['mark']) .')</small>
+                    </h5>
+                    <!-- Question should change as well -->
+                    <p class="card-text">'. $question['question'] .'</p>
 
-          <!-- How a theory question card should look. -->
-          <div class="card my-3">
-            <div class="card-body">
-              <!-- Question no and marks should change -->
-              <h5 class="card-title">
-                Question 1
-                <small>(10 Marks)</small>
-              </h5>
-              <!-- Question should change as well -->
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h6 class=" mb-2">Response</h6>
+                    <!-- Textarea name should change -->
+                    <textarea class="form-control" name="response'. $question['question_no'] .'" maxlength="256"></textarea>
+                  </div>
+                </div>
+                ';
+              }
+            }
+          ?>
 
-              <h6 class=" mb-2">Response</h6>
-              <!-- Textarea name should change -->
-              <textarea class="form-control" name="response1" maxlength="256"></textarea>
-            </div>
-          </div>
+<?php
+          // <!-- Each card is a question group -->
 
-          <div class="card my-3">
-            <div class="card-body">
-              <h5 class="card-title">
-                Question 2
-                <small>(10 Marks)</small>
-              </h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
-              <h6 class=" mb-2">Response</h6>
-              <textarea class="form-control" name="response2" maxlength="256"></textarea>
-            </div>
-          </div>
+          // <!-- How a theory question card should look. -->
+          // <div class="card my-3">
+          //   <div class="card-body">
+          //     <!-- Question no and marks should change -->
+          //     <h5 class="card-title">
+          //       Question 1
+          //       <small>(10 Marks)</small>
+          //     </h5>
+          //     <!-- Question should change as well -->
+          //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+          //     <h6 class=" mb-2">Response</h6>
+          //     <!-- Textarea name should change -->
+          //     <textarea class="form-control" name="response1" maxlength="256"></textarea>
+          //   </div>
+          // </div>
+
+          // <div class="card my-3">
+          //   <div class="card-body">
+          //     <h5 class="card-title">
+          //       Question 2
+          //       <small>(10 Marks)</small>
+          //     </h5>
+          //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+          //     <h6 class=" mb-2">Response</h6>
+          //     <textarea class="form-control" name="response2" maxlength="256"></textarea>
+          //   </div>
+          // </div>
+
+          ?>
 
 
           <!-- Place the user ID and exam ID in the values below -->
-          <input type="hidden" name="userID" value="">
-          <input type="hidden" name="examID" value="">
+          <input type="hidden" name="userID" value="<?php echo $userID; ?>">
+          <input type="hidden" name="examID" value="<?php echo $examID; ?>">
 
           <button type="submit" name="submit" class="btn btn-success">Submit</button>
         </form>
