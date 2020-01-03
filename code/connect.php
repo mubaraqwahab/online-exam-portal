7dbdcc0ecac5f89bb55a061b7d8b487a1b063818
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-03 23:29:33 +0100

    Add content to feedback.php

--- code/connect.php
+++ code/connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db_name = "exam_portal2";
+$db_name = "exam_portal";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db_name);
