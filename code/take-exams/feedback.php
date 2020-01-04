9b34c44ff06a63a1422ea1806b8eec9667a8e32f
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-04 08:37:09 +0100

    update feedback #1

--- code/take-exams/feedback.php
+++ code/take-exams/feedback.php
@@ -7,6 +7,14 @@ require_once '../connect.php';
 $userID = $_SESSION['userID'];
 $profilePicture = $_SESSION['profilePicture'];
 
+
+// Get exam details
+$examID  = $_GET['examID'];
+$sql = "SELECT * FROM exam e INNER JOIN course c ON c.course_code = e.course_code WHERE e.exam_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('i', $examID);
+$stmt->execute();
+$exam = ($stmt->fetch_result())->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -51,7 +59,7 @@ $profilePicture = $_SESSION['profilePicture'];
               if ($feedback == 'success') {
                 // print big success
                 echo '<div class="text-success mb-2"><i class="fas fa-check-circle fa-10x"></i></div>';
-                echo '<p class="text-success"><strong>Your CSC303 (Web) Midterm has been submitted successfully.</strong></p>';
+                echo '<p class="text-success"><strong>Your '. $exam['course_code']. ' ('. $exam['course_name'] .') '. $exam['title'] .' has been submitted successfully.</strong></p>';
 
               } else if ($feedback == 'failure') {
                 // print big failure
@@ -59,7 +67,7 @@ $profilePicture = $_SESSION['profilePicture'];
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fas fa-times fa-stack-1x fa-inverse"></i>
                 </span>';
-                echo '<p class="text-danger"><strong>Your CSC303 (Web) Midterm could not be submitted. The exam might have been closed</strong></p>';
+                echo '<p class="text-danger"><strong>Your '. $exam['course_code']. ' ('. $exam['course_name'] .') '. $exam['title'] .' could not be submitted. The exam might have been closed</strong></p>';
               }
             }
 
