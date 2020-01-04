a18e49382c6ff61d472085163542430332cf1aa6
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-04 09:30:44 +0100

    Update status on exam start done for fill

--- code/take-exams/fill-exam.php
+++ code/take-exams/fill-exam.php
@@ -32,14 +32,14 @@ $examID = $_GET['examID'];
 $typeID = 2;
 
 // Get the exam details from database
-// Validate the student ID, exam ID and type ID as well
+// Validate the student ID, exam ID and type ID, and assignment status as well
 $headerSql = "SELECT e.*, c.course_name, t.value AS type, CONCAT(u.first_name, ' ', u.last_name) AS instructor
   FROM `exam` AS e
   INNER JOIN user AS u ON u.user_id = e.instructor_id
   INNER JOIN `course` AS c ON c.course_code = e.course_code
   INNER JOIN exam_type AS t ON t.type_id = e.type_id
   WHERE e.exam_id = ? AND e.type_id = ?
-  AND EXISTS (SELECT 1 FROM exam_assignment WHERE exam_id = ? AND assignee_id = ?)";
+  AND EXISTS (SELECT 1 FROM exam_assignment WHERE exam_id = ? AND assignee_id = ? AND status_id = 3)";
 
 $headerStmt = $conn->prepare($headerSql);
 $headerStmt->bind_param('iisi', $examID, $typeID, $examID, $userID);
@@ -63,6 +63,18 @@ $questionStmt = $conn->prepare($questionSql);
 $questionStmt->bind_param('i', $examID);
 $questionStmt->execute();
 $questionResult = $questionStmt->get_result();
+
+// Let the db know the student has started the exam
+$statusSql = "UPDATE exam_assignment SET status_id = 4 WHERE exam_id = ? AND assignee_id = ?";
+
+$statusStmt = $conn->prepare($statusSql);
+$statusStmt->bind_param('is', $examID, $userID);
+$statusStmt->execute();
+
+if ($conn->affected_rows != 1) {
+  showError('Page currently unavailable. Please try again later.');
+  exit;
+}
 ?>
 
 <body>
