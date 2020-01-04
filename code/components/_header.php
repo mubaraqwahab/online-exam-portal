1805bb7efc4f3215f6daa4d8e7453a9f9dc2b2e4
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-04 18:04:35 +0100

    Add main header

--- code/components/_header.php
+++ code/components/_header.php
@@ -1,4 +1,4 @@
-<header class="text-center py-2">
+<header class="text-center py-2" id="main-header">
   <a href="https://nileuniversity.edu.ng/" class="d-inline-flex content-center">
     <img src="../img/nile.png" height="40px" width="40px">
     <span class="h5 ml-2 mb-0">ONLINE EXAM PORTAL</span>
