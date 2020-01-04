556c9382df7d4e4ff5ad0056efe0df8716acb3aa
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-04 08:41:07 +0100

    Update feedback #2

--- code/take-exams/feedback.php
+++ code/take-exams/feedback.php
@@ -8,13 +8,6 @@ $userID = $_SESSION['userID'];
 $profilePicture = $_SESSION['profilePicture'];
 
 
-// Get exam details
-$examID  = $_GET['examID'];
-$sql = "SELECT * FROM exam e INNER JOIN course c ON c.course_code = e.course_code WHERE e.exam_id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param('i', $examID);
-$stmt->execute();
-$exam = ($stmt->fetch_result())->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +24,23 @@ $exam = ($stmt->fetch_result())->fetch_assoc();
   <link href="../css/simple-sidebar.css" rel="stylesheet">
 
 </head>
+
+<?php
+// Get exam details
+$examID  = $_GET['examID'];
+$sql = "SELECT * FROM exam e INNER JOIN course c ON c.course_code = e.course_code WHERE e.exam_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('i', $examID);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows != 1) {
+  showError('Page unavailable.');
+  exit;
+}
+
+$exam = $result->fetch_assoc();
+?>
 
 <body>
 
@@ -68,7 +78,11 @@ $exam = ($stmt->fetch_result())->fetch_assoc();
                   <i class="fas fa-times fa-stack-1x fa-inverse"></i>
                 </span>';
                 echo '<p class="text-danger"><strong>Your '. $exam['course_code']. ' ('. $exam['course_name'] .') '. $exam['title'] .' could not be submitted. The exam might have been closed</strong></p>';
+              } else {
+                showError('Page unavailable.');
               }
+            } else {
+              showError('Page unavailable.');
             }
 
           ?>
