990a2f561448b67f6ac6b7b81d5d58cc38e0cd0c
Esat <yamanesad.ey@gmail.com>
2020-01-04 01:38:42 +0100

    Submit exam fixed

--- code/take-exams/_submit-exam.php
+++ code/take-exams/_submit-exam.php
@@ -2,26 +2,61 @@
 
 include '../connect.php';
 
-
-// if submit button has been set
-// Get examID, userID from post
-// Check db for exam type
-
-// If exam is theory, set output table to theory_response
-// If exam is fill-in, set output table to fill_in_response
-// If exam is obj, set output table to multi_response
-
-// for each question:
-  // If exam is obj,
-    // Grade response
-
-  // Send response to output table
-  // Details: exam id, question no, assignee id, response
+if(isset($_POST['submit'])){
+  $examID = $_POST['examID'];
+  $userID = $_POST['userID'];
 
 
+  $sql = "SELECT type_id, no_of_questions FROM exam WHERE exam_id = $examID";
+  $result = mysqli_query($conn, $sql);
+  $exam = mysqli_fetch_assoc($result);
+  $typeID = $exam['type_id'];
+  $noOfQuestions = $exam['no_of_questions'];
+
+  if($typeID == 1){
+
+    $outputTable = "multi_choice_response";
+  }
+  else if($typeID == 2){
+    $outputTable = "fill_in_response";
+  }
+  else if($typeID == 3){
+    $outputTable = "theory_response";
+  }
+  
+  for($i=1; $i<=$noOfQuestions; $i++){
+    $score = 'NULL';
+    $response = $_POST['response' . $i];
+    if($typeID == 1){
+      $sql3 = "SELECT correct_answer, mark FROM multi_choice_question WHERE exam_id = $examID AND question_number = $i";
+      $result3 = mysqli_query($conn, $sql3);
+      $exam3 = mysqli_fetch_assoc($result3);
+      $correctAnswer = $exam3['correct_answer'];
+
+      if($response == $correctAnswer){
+        $score = $exam3['mark'];
+        
+
+      }
+      else{
+        $score = 0;
+      }
+    }
+    $sql2 = "INSERT INTO $outputTable (exam_id, question_no, assignee_id, response, score) VALUES ($examID, $i, '$userID', '$response', $score)";
+    $result2 = mysqli_query($conn, $sql2);
+  }
+$sql4 = "UPDATE exam_assignment SET status_id = $newStatus WHERE exam_id = $examID AND assignee_id = $userID";
+$result4 = mysqli_query($conn, $sql4);
+$newStatus = $typeID == 1 ? 6 : 5;
 
 
+}
 
-header("Location: feedback.php?examID=&feedback=");
+if (mysqli_affected_rows($conn) != 0){
+  header("Location: feedback.php?examID=$examID&feedback=success");
+}
+else{
+  header("Location: feedback.php?examID=$examID&feedback=failure");
+}
 
 ?>
