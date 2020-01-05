7a1444ec24ec91e05fde7663ea61225cd673fbb5
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-05 12:24:46 +0100

    Add noti count + color

--- code/components/_sidebar.php
+++ code/components/_sidebar.php
@@ -1,8 +1,19 @@
+<?php
+
+// Get no of new notifications
+$sql = "SELECT notification_id FROM notification WHERE recipient_id = $userID AND status_id = 1";
+$notiCount = ($conn->query($sql))->num_rows;
+
+?>
+
 <div class="bg-light border-right" id="sidebar-wrapper">
   <div class="sticky-top pt-2">
     <h5 class="sidebar-heading">Dashboard</h5>
     <div class="list-group list-group-flush">
-      <a href="<?php echo ROOT_DIR . 'notifications/' ?>" class="list-group-item list-group-item-action bg-light" id="notifications">Notifications</a>
+      <a href="<?php echo ROOT_DIR . 'notifications/' ?>" class="list-group-item list-group-item-action bg-light" id="notifications">
+        Notifications
+        <?php echo $notiCount == 0 ? "" : " <span class='badge badge-info'>$notiCount</span>"; ?>
+      </a>
 
       <span class="list-group-item bg-light mt-3"><b>For Instructors</b></span>
       <a href="<?php echo ROOT_DIR . 'create-exam/' ?>" class="list-group-item list-group-item-action bg-light" id="create-exam">Create Exam</a>
