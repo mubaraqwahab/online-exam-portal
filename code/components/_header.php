90401beb693e251bbc9a32794e95ab8fc30e9df7
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-01-05 19:11:28 +0100

    Update header

--- code/components/_header.php
+++ code/components/_header.php
@@ -1,6 +1,5 @@
-<header class="text-center py-2" id="main-header">
-  <a href="https://nileuniversity.edu.ng/" class="d-inline-flex content-center">
-    <img src="../img/nile.png" height="40px" width="40px">
-    <span class="h5 ml-2 mb-0">ONLINE EXAM PORTAL</span>
+<header class="text-center py-2 bg-primary" id="main-header">
+  <a href="https://nileuniversity.edu.ng/" class="d-inline-flex text-decoration-none text-light">
+    <span class="h5 mb-0">ONLINE EXAM PORTAL</span>
   </a>
 </header>
